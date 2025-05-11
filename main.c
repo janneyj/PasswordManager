f0f6d68ce53c64fd52dae0f71a40968c750c5fff
Joy Janney <143513601+janneyj@users.noreply.github.com>
2025-05-11 21:53:54 +0000

    basic functionality works

--- main.c
+++ main.c
@@ -18,51 +18,57 @@ struct loginfo{
    char password[lpass];
 };
 
-
 //This function is supposed to recieve a part of a struct that is a string and update the member
-bool MemberUpdate(FILE *ptr, char updateName[1028], char member[]){
+bool MemberUpdate(char updateName[1028], char member[]){
    printf("Input %s: ", updateName);
    scanf("%1028s", member);//use str copy
    printf("--%s--\n", member);
    return false;
 }
 
-int get_name(char *name){
-   char user_input[1048];
-
-   printf("Input the url\n");
-   scanf("%1048s", user_input);
+int get_name(char *URL, char* domain){
    int start = 0;
    int end = 0;
-
-   char start_phrase[5] = "www.\0";
-   char end_phrase[5][5] = {".com\0", ".gov\0", ".org\0", ".edu\0", ".mil\0"};
-   char check[5];
-   check[4] = '\0';
-   int result = 5;
+   const char start_phrase[3][9] = {"www.\0", "https://\0", "http://\0"};
+   const char end_phrase[5][5] = {".com", ".gov", ".org", ".edu", ".mil"};
    
-   for(int i =3; user_input[i]; i++){
-      for(int n = 0; n < 4; n++){
-         check[n] = user_input[i-(3-n)];
-      }
-      result = strcmp(check,start_phrase);
-      if (result==0){
-         start = i+1;}
-      
-      if (((check[3] == 47) && (check[4] == 0)) || ((check[2] == 47) && (check[3] != 47))){
-         end = i-4;
-      }
+   if(strlen(URL)==0)
+   {
+      printf("ERROR: null string\nCalled by get_name\n");
    }
-   if (end == 0){
-      printf("URL was faulty\n");
-      return -1;
-   }
-   for(int i = start; i < end; i++){
-
-      *name = user_input[i];
-      name++;
+   for(int n = 0; n < 3 && start == 0; n++)
+   {
+      char compare[9];
+      int t = strlen(start_phrase[n]);
+      for(int i = 0; i < strlen(start_phrase[n]); i++)
+      {
+         compare[i] = URL[i];
+      }
+      if(strncmp(compare, start_phrase[n], strlen(start_phrase[n])) == 0)
+         start = n+strlen(start_phrase[n]);
    }
 
+   for(int n = 0; n < 5 && end == 0; n++)
+   {
+      char compare[5];
+      int begin = strlen(URL)-strlen(end_phrase[n]);
+      for(int i = begin; i <= strlen(URL); i++)
+      {
+         compare[i-begin] = URL[i];
+      }
+      if(strcmp(compare, end_phrase[n]) == 0)
+      {
+         end = begin;
+      }
+   }
+   int n;
+   for(int i = start; i < end; i++)
+   {
+      n=i-start;
+      domain[n] = URL[i];
+
+   }
+   domain[n+1] = 0;
    fflush(stdin);
    return 0;
 }
@@ -78,13 +84,18 @@ Description:
       creates a file consisting of one struct where the file name is the domain name
 */
 int create(){
-   char domain[2048];
-   for (int i =0; i < 2048; i++){
-      domain[i] = 0;
-   }
-   get_name(&domain[0]);
+   //Adding the information to the struct
+   struct loginfo current = {"_blank\0", "_blank\0", "_blank\0", "_blank\0"};
+   
+   MemberUpdate("username", current.username);
+   MemberUpdate("password", current.password);
+   MemberUpdate("url", current.url);
+   get_name(&current.url[0], &current.domain[0]);
 
-   FILE *fptr = fopen (domain, "w");
+   char temp[2048];
+   strcpy(temp, current.domain);
+   
+   FILE *fptr = fopen (strcat(temp, ".txt"), "w");
 
    //check to see if the file has been correctly opened, if not abort
    if(fptr==NULL)
@@ -93,62 +104,12 @@ int create(){
       return -1;
    }
 
-   //Adding the information to the struct
-   struct loginfo current = {"_blank", "_blank", "_blank", "_blank"};
-   /*printf("Please enter the username: ");
-   scanf("%25s", current.username);*/
-   MemberUpdate(fptr, "username", current.username);
-   MemberUpdate(fptr, "password", current.password);
-   MemberUpdate(fptr, "url", current.url);
-   fprintf(fptr, "url:\t\t{%s}\ndomain:\t\t{%s}\nusername:\t{%s}\npassword:\t{%s}\n", current.url, current.domain, current.username, current.password);
+   fprintf(fptr, "url:\t\t{%s}\ndomain:\t\t{%s}\nusername:\t{%s}\npassword:\t{%s}\n",
+      current.url, current.domain, current.username, current.password);
    fclose(fptr);
    fflush(stdin);
    return 0;
 }
-
-
-
-//This function should ask what needs to be updated and then requests what the new update should be
-//FIXME retrieve and save it. Currently not retrieving or saving just updating a struct
-int Update(){
-   //file is not opening
-   char FileName[2048];
-   get_name(&FileName[0]);
-   FILE *fptr = fopen(FileName, "w");
-   if(fptr==NULL)
-   {
-      printf("Update: Unable to open file.\n\n\n\n");
-      return -1;
-   }
-   struct loginfo current;
-   bool correct = true;
-   char answer[20];
-   char update[lurl];
-   printf("What field needs to be update? ");
-   scanf("%10s", answer);
-   if(strcmp(answer,"domain")==0){
-      MemberUpdate(fptr, answer, current.domain);
-   }
-   else if (strcmp(answer,"url")==0){
-      MemberUpdate(fptr, answer, current.url);
-   }
-   else if (strcmp(answer,"username")==0){
-      MemberUpdate(fptr, answer, current.username);
-   }
-   else if (strcmp(answer,"password")==0){
-      MemberUpdate(fptr, answer, current.password);
-   
-   }
-      
-   for(int i=0; answer[i]!='\0'; i++){
-      answer[i]=tolower(answer[i]);
-   }
-   fflush(fptr);//file not being saved correctly
-
-   fclose(fptr);
-   fflush(stdin);
-   return 0;
-   }
 /*
 Name: delete
 Parameters: void
@@ -159,7 +120,7 @@ Description: This will ask the user for the file to be deleted, deletes the file
 */   
 int delete(void){
    char FileName[2048];
-   get_name(&FileName[0]);
+   get_name(&FileName[0], &FileName[0]);
    if(remove(FileName) != 0){
       printf("Did not delete file.");
       return -1;
@@ -178,12 +139,11 @@ Return:
       -1 - int: if there is a problem
 Description: This will read from the file the parts of the loginfo struct and save it to a struct
 */
-int retrieve(char name[150], struct loginfo* info){
+int retrieve(char FileName[], struct loginfo* info){
    //opening the file and checking to make sure that the file opened correctly
-   char FileName[2048];
-   get_name(&FileName[0]);
    FILE *fptr;
    char retrieve[2048];
+   strcat(FileName, ".txt");
    fptr = fopen(FileName, "r");
    if(fptr==NULL){
       printf("Retrieve: Unable to open file\n");
@@ -191,22 +151,83 @@ int retrieve(char name[150], struct loginfo* info){
    }
    //the first part read is the name of the struct that it is a part of so it should be skipped
    //the second part is the actual part that needs to be saved
+   char ch[1];
    fscanf(fptr, "%2048s", retrieve);
+   fscanf(fptr, "%1s", ch);
    fscanf(fptr, "%2048s", info->url);
+   info->url[strlen(info->url)-1] = 0;
 
    fscanf(fptr, "%2048s", retrieve);
+   fscanf(fptr, "%1s", ch);
    fscanf(fptr, "%2048s", info->domain);
+   info->domain[strlen(info->domain)-1] = 0;
 
    fscanf(fptr, "%2048s", retrieve);
+   fscanf(fptr, "%1s", ch);
    fscanf(fptr, "%2048s", info->username);
+   info->username[strlen(info->username)-1] = 0;
 
    fscanf(fptr, "%2048s", retrieve);
+   fscanf(fptr, "%1s", ch);
    fscanf(fptr, "%2048s", info->password);
+   info->password[strlen(info->password)-1] = 0;
 
    fclose(fptr);
    fflush(stdin);
    return 0;
 }
+
+//This function should ask what needs to be updated and then requests what the new update should be
+//FIXME retrieve and save it. Currently not retrieving or saving just updating a struct
+int Update(struct loginfo* current){
+   //file is not opening
+   char FileName[2048];
+   printf("Input URL: ");
+   scanf("%2048s", FileName);
+   get_name(&FileName[0], &FileName[0]);
+   retrieve(FileName, current);
+   if(remove(FileName) != 0)
+   {
+      printf("ERROR: Update could not remove old file\n");
+      return -1;
+   }
+   FILE *fptr = fopen(FileName, "w");
+   if(fptr==NULL)
+   {
+      printf("Update: Unable to open file.\n\n\n\n");
+      return -1;
+   }
+   bool correct = true;
+   char answer[20];
+   char update[lurl];
+   printf("What field needs to be update? ");
+   scanf("%10s", answer);
+   if(strcmp(answer,"domain")==0){
+      MemberUpdate(answer, current->domain);
+   }
+   else if (strcmp(answer,"url")==0){
+      MemberUpdate(answer, current->url);
+   }
+   else if (strcmp(answer,"username")==0){
+      MemberUpdate(answer, current->username);
+   }
+   else if (strcmp(answer,"password")==0){
+      MemberUpdate(answer, current->password);
+   
+   }
+      
+   for(int i=0; answer[i]!='\0'; i++){
+      answer[i]=tolower(answer[i]);
+   }
+   fflush(fptr);//file not being saved correctly
+   fprintf(fptr, "url:\t\t{%s}\ndomain:\t\t{%s}\nusername:\t{%s}\npassword:\t{%s}\n",
+      current->url, current->domain, current->username, current->password);
+   fclose(fptr);
+   fflush(stdin);
+   return 0;
+   }
+
+
 //This funciton allows the user to access a step by step guide to using the program
 void help(){
    printf("Press\n1 - For help with saving a password\n2 - For help with viewing a password, username, or URL\n");
@@ -248,6 +269,12 @@ void help(){
    }
 }
 
+void printLogInfo(struct loginfo* info)
+{
+   printf("URL: %s\nDomain: %s\nUsername: %s\nPassword: %s\n",
+      info->url, info->domain, info->username, info->password);
+}
+
 int main() {
    char option = true;
    char user_input[2];
@@ -264,33 +291,42 @@ int main() {
       printf("Please input C for create, R for retrieve, U for Update, D for delete, and H for Help.\n");
       printf("To end the program press any other key\n");
       scanf("%1s", user_input);
-      if (strcmp(user_input, "C") == 0){
+      if (strcmp(user_input, "C") == 0||strcmp(user_input, "c")==0){
          if( create() == -1)
             printf("Something horrible happened\n");
       }
-      else if (strcmp(user_input, "U")==0){
-         if( Update() == -1)
+      else if (strcmp(user_input, "U")==0||strcmp(user_input, "u")==0){
+         if( Update(&current) == -1)
             printf("Something horrible happened\n");
       }
-      else if (strcmp(user_input, "R")==0){
-         if( retrieve("joy", &current) == -1)
+      else if (strcmp(user_input, "R")==0||strcmp(user_input, "r")==0){
+         printf("Input URL: ");
+         char temp[lurl];
+         scanf("%2048s", temp);
+         get_name(&temp[0], &temp[0]);
+         if( retrieve(temp, &current) == -1)
             printf("Something horrible happened\n");
+         else
+            printLogInfo(&current);
       }
-      else if (strcmp(user_input, "D")==0){
+      else if (strcmp(user_input, "D")==0||strcmp(user_input, "d")==0){
          if( delete() == -1)
             printf("Something horrible happened\n");
       }
-      else if (strcmp(user_input, "H")==0){
+      else if (strcmp(user_input, "H")==0||strcmp(user_input, "h")==0){
          char domain[1048];
          help();
       }
       else
+      {
          option = false;
+      }
    }while (option == true);
 
-   printf("Retrieving file\n");
-   retrieve("joy", &current);
-   printf("username: %s\npassword: %s\nurl: %s\ndomain: %s\n", current.username, current.password, current.url, current.domain);
-
+   if(option == true){
+      printf("Retrieving file\n");
+      retrieve("joy", &current);
+      printf("username: %s\npassword: %s\nurl: %s\ndomain: %s\n", current.username, current.password, current.url, current.domain);
+   }
    return 0;
 }
